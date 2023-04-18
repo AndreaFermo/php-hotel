ca3c8adb32cefe980c4c7c96d1954d625bb47017
Andrea Fermo <andreafermo.sd@gmail.com>
2023-04-18 15:37:36 +0200

    esercizio base concluso

--- index.php
+++ index.php
@@ -38,9 +38,49 @@
 
     ];
 
-    foreach ($hotels as $key => $hotels) {
-        foreach($hotels as $hotel) {
-            echo $hotel . '<br/>';
-        };
-    };
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotels</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<table class="table table-primary table-striped table-hover container-lg">
+    <thead>
+        <tr>
+            <?php
+                foreach($hotels[0] as $key => $hotel) {
+                    echo "<th>".str_replace("_", " ", ucfirst($key))."</th>";
+                };
+            ?>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+foreach ($hotels as $key => $hotel) {
+    echo "<tr>";
+    if ($hotel['parking']) {
+        $hotel['parking'] = 'yes';
+    } else {
+        $hotel['parking'] = 'no';
+    };
+    foreach($hotel as $value) {
+        echo "<td>".$value."</td>";
+       
+    };
+    echo "</tr>";
+};
+    ?>
+    </tbody>
+</table>
+   
+    
+</body>
+</html>
